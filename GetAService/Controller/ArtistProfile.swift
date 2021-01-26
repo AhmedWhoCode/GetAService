cc985212b5ea2ea6965a151ece07e87585d4d9ba
AhmedWhoCodde <atehseen044@gmail.com>
2021-01-26 19:34:03 +0500

    Ui bugs fix and some modifications
    added shadows

--- GetAService/Controller/ArtistProfile.swift
+++ GetAService/Controller/ArtistProfile.swift
@@ -26,38 +26,73 @@ class ArtistProfile: UIViewController {
     
     func designingView() {
         ///MARK: - designing views
-        artistNameTextField.layer.cornerRadius = 7
-        artistNameTextField.layer.borderWidth = 1
-        artistNameTextField.layer.borderColor = UIColor.black.cgColor
+//        artistNameTextField.layer.cornerRadius = 10
+//        artistNameTextField.layer.borderWidth = 0.1
+//        artistNameTextField.layer.borderColor = UIColor.black.cgColor
         
         //shadow
         artistNameTextField.layer.shadowColor = UIColor.gray.cgColor
-        artistNameTextField.layer.shadowOpacity = 1
+        artistNameTextField.layer.shadowOpacity = 0.5
         artistNameTextField.layer.shadowOffset = CGSize.zero
         artistNameTextField.layer.shadowRadius = 7
   
         //To apply padding
-        let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: artistNameTextField.frame.height))
+        let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: artistNameTextField.frame.height))
         artistNameTextField.leftView = paddingView
         artistNameTextField.leftViewMode = UITextField.ViewMode.always
         
+        //  2nd view
+        //shadow
+        artistAddressTextField.layer.shadowColor = UIColor.gray.cgColor
+        artistAddressTextField.layer.shadowOpacity = 0.5
+        artistAddressTextField.layer.shadowOffset = CGSize.zero
+        artistAddressTextField.layer.shadowRadius = 7
+
+        //To apply padding
+       let paddingView2 : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: artistAddressTextField.frame.height))
+        artistAddressTextField.leftView = paddingView2
+        artistAddressTextField.leftViewMode = UITextField.ViewMode.always
+
+
+        // 3rd view
+        //shadow
+        artistEmailTextField.layer.shadowColor = UIColor.gray.cgColor
+        artistEmailTextField.layer.shadowOpacity = 0.5
+        artistEmailTextField.layer.shadowOffset = CGSize.zero
+        artistEmailTextField.layer.shadowRadius = 7
+
+        //To apply padding
+        let paddingView3 : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: artistAddressTextField.frame.height))
+        artistEmailTextField.leftView = paddingView3
+        artistEmailTextField.leftViewMode = UITextField.ViewMode.always
+
+        //4rth view
+
+        //shadow
+        artistPriceTextField.layer.shadowColor = UIColor.gray.cgColor
+        artistPriceTextField.layer.shadowOpacity = 0.5
+        artistPriceTextField.layer.shadowOffset = CGSize.zero
+        artistPriceTextField.layer.shadowRadius = 7
+
+        //To apply padding
+        let paddingView4 : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: artistAddressTextField.frame.height))
+        artistPriceTextField.leftView = paddingView4
+        artistPriceTextField.leftViewMode = UITextField.ViewMode.always
+
+       // 5th view
+
+        //shadow
+        artistNumberTextField.layer.shadowColor = UIColor.gray.cgColor
+        artistNumberTextField.layer.shadowOpacity = 0.5
+        artistNumberTextField.layer.shadowOffset = CGSize.zero
+        artistNumberTextField.layer.shadowRadius = 7
+
+        //To apply padding
+        let paddingView5 : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: artistAddressTextField.frame.height))
+        artistNumberTextField.leftView = paddingView5
+        artistNumberTextField.leftViewMode = UITextField.ViewMode.always
         
-        artistAddressTextField.layer.cornerRadius = 15
-        artistAddressTextField.layer.borderWidth = 1
-        artistAddressTextField.layer.borderColor = UIColor.black.cgColor
-        
-        artistEmailTextField.layer.cornerRadius = 15
-        artistEmailTextField.layer.borderWidth = 1
-        artistEmailTextField.layer.borderColor = UIColor.black.cgColor
-        
-        artistPriceTextField.layer.cornerRadius = 15
-        artistPriceTextField.layer.borderWidth = 1
-        artistPriceTextField.layer.borderColor = UIColor.black.cgColor
-        
-        artistNumberTextField.layer.cornerRadius = 15
-        artistNumberTextField.layer.borderWidth = 1
-        artistNumberTextField.layer.borderColor = UIColor.black.cgColor
-        
+        //sth view
         submitButton.layer.cornerRadius = 20
         submitButton.layer.borderWidth = 1
         submitButton.layer.borderColor = UIColor.black.cgColor
