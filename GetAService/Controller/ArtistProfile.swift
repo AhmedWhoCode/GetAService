63275f912874539e873ab7272ff5b2e758895e00
AhmedWhoCodde <atehseen044@gmail.com>
2021-01-26 17:48:25 +0500

    some changes

--- GetAService/Controller/ArtistProfile.swift
+++ GetAService/Controller/ArtistProfile.swift
@@ -10,6 +10,14 @@ import UIKit
 class ArtistProfile: UIViewController {
 
     @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var artistNameTextField: UITextField!
+    @IBOutlet weak var artistAddressTextField: UITextField!
+    @IBOutlet weak var artistEmailTextField: UITextField!
+    @IBOutlet weak var artistPriceTextField: UITextField!
+    @IBOutlet weak var artistNumberTextField: UITextField!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var genderChooser: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         designingView()
@@ -18,6 +26,38 @@ class ArtistProfile: UIViewController {
     
     func designingView() {
         ///MARK: - designing views
+        artistNameTextField.layer.cornerRadius = 7
+        artistNameTextField.layer.borderWidth = 1
+        artistNameTextField.layer.borderColor = UIColor.black.cgColor
+        
+        //shadow
+        artistNameTextField.layer.shadowColor = UIColor.gray.cgColor
+        artistNameTextField.layer.shadowOpacity = 1
+        artistNameTextField.layer.shadowOffset = CGSize.zero
+        artistNameTextField.layer.shadowRadius = 7
+  
+        //To apply padding
+        let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: artistNameTextField.frame.height))
+        artistNameTextField.leftView = paddingView
+        artistNameTextField.leftViewMode = UITextField.ViewMode.always
+        
+        
+        artistAddressTextField.layer.cornerRadius = 15
+        artistAddressTextField.layer.borderWidth = 1
+        artistAddressTextField.layer.borderColor = UIColor.black.cgColor
+        
+        artistEmailTextField.layer.cornerRadius = 15
+        artistEmailTextField.layer.borderWidth = 1
+        artistEmailTextField.layer.borderColor = UIColor.black.cgColor
+        
+        artistPriceTextField.layer.cornerRadius = 15
+        artistPriceTextField.layer.borderWidth = 1
+        artistPriceTextField.layer.borderColor = UIColor.black.cgColor
+        
+        artistNumberTextField.layer.cornerRadius = 15
+        artistNumberTextField.layer.borderWidth = 1
+        artistNumberTextField.layer.borderColor = UIColor.black.cgColor
+        
         submitButton.layer.cornerRadius = 20
         submitButton.layer.borderWidth = 1
         submitButton.layer.borderColor = UIColor.black.cgColor
