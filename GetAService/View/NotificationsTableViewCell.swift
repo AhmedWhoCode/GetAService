9a09d58e6e133d5e94bf644e1d9f8b5e3ae9092b
AhmedWhoCodde <atehseen044@gmail.com>
2021-01-28 22:25:55 +0500

    make round image view

--- GetAService/View/NotificationsTableViewCell.swift
+++ GetAService/View/NotificationsTableViewCell.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class NotificationsTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var requestedByLabel: UILabel!
-    @IBOutlet weak var typeOfServiceLabel: UILabel!
-    @IBOutlet weak var availabileLabel: UILabel!
+    @IBOutlet weak var customerName: UILabel!
+    @IBOutlet weak var customerCountry: UILabel!
+    @IBOutlet weak var customerImage: UIImageView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        //customerImage.layer.borderWidth = 1
+         customerImage.layer.masksToBounds = true
+         customerImage.layer.borderColor = UIColor.black.cgColor
+        customerImage.layer.cornerRadius = customerImage.frame.size.height/2
+         customerImage.contentMode = .scaleAspectFill
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -24,3 +32,20 @@ class NotificationsTableViewCell: UITableViewCell {
     }
     
 }
+//extension UIImage {
+//    var circleMask: UIImage? {
+//        let square = CGSize(width: min(size.width, size.height), height: min(size.width, size.height))
+//        let imageView = UIImageView(frame: .init(origin: .init(x: 0, y: 0), size: square))
+//        imageView.contentMode = .scaleAspectFill
+//        imageView.image = self
+//        imageView.layer.cornerRadius = square.width/2
+//        imageView.layer.borderColor = UIColor.white.cgColor
+//        imageView.layer.borderWidth = 5
+//        imageView.layer.masksToBounds = true
+//        UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, false, scale)
+//        defer { UIGraphicsEndImageContext() }
+//        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+//        imageView.layer.render(in: context)
+//        return UIGraphicsGetImageFromCurrentImageContext()
+//    }
+
